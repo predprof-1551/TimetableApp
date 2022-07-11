ac234ba445d8a0359f909c9d638078280029860f
Denis Voitenko <denis@denvot.dev>
2022-07-11 18:16:36 +0300

    Контекст БД

---
+++ TimetableApp/Data/Database/Activity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TimetableApp.Data.Database
+{
+    public partial class Activity
+    {
+        public long Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string? Description { get; set; }
+        public byte Rcolor { get; set; }
+        public byte Gcolor { get; set; }
+        public byte Bcolor { get; set; }
+        public TimeSpan TimeStart { get; set; }
+        public long TimetableId { get; set; }
+        public TimeSpan TimeEnd { get; set; }
+
+        public virtual Timetable Timetable { get; set; } = null!;
+    }
+}
---
+++ TimetableApp/Data/Database/Timetable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TimetableApp.Data.Database
+{
+    public partial class Timetable
+    {
+        public Timetable()
+        {
+            Activities = new HashSet<Activity>();
+        }
+
+        public long Id { get; set; }
+        public string? Description { get; set; }
+        public DateTime Date { get; set; }
+        public int RetryRate { get; set; }
+
+        public virtual ICollection<Activity> Activities { get; set; }
+    }
+}
